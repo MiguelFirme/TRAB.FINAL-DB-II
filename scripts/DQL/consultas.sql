61fe5c9089fc4ec4ff8e3946f668a11f94bc111e
João Gustavo Vitali <joaogustavovitali09@gmail.com>
2025-06-19 16:07:17 -0300

    Update consultas.sql
    Adicionei novas consultas na pergunta 1, 2 e 5

--- scripts/DQL/consultas.sql
+++ scripts/DQL/consultas.sql
@@ -1,16 +1,27 @@
---1-Quais clientes tiveram mais de um sinistro nos últimos 12 meses?
-SELECT Clientes.id_cliente, Clientes.nome, COUNT(Sinistros.id_sinistro) AS qtde_sinistros 
-FROM Clientes 
-INNER JOIN Aparelhos ON Clientes.id_cliente = Aparelhos .id_cliente 
-INNER JOIN Apolices ON Aparelhos.id_aparelho = Apolices .id_aparelho 
-INNER JOIN Sinistros ON Apolices .id_apolice = Sinistros.id_apolice 
-WHERE Sinistros .data_ocorrencia >= DATEADD(MONTH, -12, GETDATE())
-GROUP BY Clientes.id_cliente, Clientes.nome
-HAVING COUNT(Sinistros.id_sinistro) > 1;
+---1-Quais clientes, que possuem apólices de plano Prata ou superior, tiveram mais de um sinistro do tipo 'Roubo' ou 'Dano', com status diferente de 'Negado', ocorridos nos últimos 12 meses?
+SELECT 
+    Clientes.nome AS NomeCliente,
+    COUNT(Sinistros.id_sinistro) AS QuantidadeSinistros
+FROM Clientes
+INNER JOIN Aparelhos ON Clientes.id_cliente = Aparelhos.id_cliente
+INNER JOIN Apolices ON Aparelhos.id_aparelho = Apolices.id_aparelho
+INNER JOIN Planos_Seguro ON Apolices.id_plano = Planos_Seguro.id_plano
+INNER JOIN Sinistros ON Apolices.id_apolice = Sinistros.id_apolice
+WHERE 
+    Planos_Seguro.nome_plano IN ('Prata', 'Ouro', 'Diamante')  
+    AND Sinistros.tipo_ocorrencia IN ('Roubo', 'Dano')         
+    AND Sinistros.status_sinistro <> 'Negado'                 
+    AND Sinistros.data_ocorrencia >= DATEADD(MONTH, -12, GETDATE()) 
+GROUP BY Clientes.nome
+HAVING COUNT(Sinistros.id_sinistro) > 1                        
+ORDER BY QuantidadeSinistros DESC;
 
---2-Quais os 5 modelos de aparelhos possuem o maior custo médio de reparo e qual a posição de cada um no ranking?
+
+--2-Quais os 5 modelos de aparelhos possuem o maior custo médio de reparo acima de 2000.00 e qual a posição de cada um no ranking?
 WITH CTE AS ( 
-    SELECT  modelo, AVG(Reparos.custo_reparo) AS custo_medio 
+    SELECT  
+        modelo, 
+        AVG(Reparos.custo_reparo) AS custo_medio 
     FROM Aparelhos 
     INNER JOIN Apolices ON Aparelhos.id_aparelho = Apolices.id_aparelho
     INNER JOIN Sinistros ON Apolices.id_apolice = Sinistros.id_apolice
@@ -18,11 +29,15 @@ WITH CTE AS (
     GROUP BY modelo
 ),
 CTE_RANK AS ( 
-    SELECT modelo, custo_medio, RANK() OVER(ORDER BY custo_medio DESC) AS posicao 
+    SELECT 
+        modelo, 
+        custo_medio, 
+        RANK() OVER (ORDER BY custo_medio DESC) AS posicao 
     FROM CTE
+    WHERE custo_medio > 2000  
 )
 SELECT * 
-FROM CTE_RANK 
+FROM CTE_RANK
 WHERE posicao <= 5;
 
 --3-Qual a média de tempo (em dias) entre a ocorrência do sinistro e a conclusão do reparo, segmentada por modelo do aparelho e plano de seguro contratado?
@@ -49,20 +64,23 @@ LEFT JOIN  Sinistros ON Apolices.id_apolice = Sinistros.id_apolice
 GROUP BY  Planos_Seguro.id_plano, Planos_Seguro.nome_plano 
 ORDER BY indice_sinistros DESC; 
 
---5-Quais clientes geraram mais despesas (reparos + reembolsos) para a seguradora?
-SELECT Clientes.id_cliente, Clientes.nome,
-    SUM(COALESCE(Reparos.custo_reparo, 0)) AS total_reparos,
-    SUM(COALESCE(Pagamentos.valor_pago, 0)) AS total_pagamentos,
-    SUM(COALESCE(Reparos.custo_reparo, 0) + COALESCE(Pagamentos.valor_pago, 0)) AS total_despesas,
-    RANK() OVER (ORDER BY SUM(COALESCE(Reparos.custo_reparo, 0) + COALESCE(Pagamentos.valor_pago, 0)) DESC) AS posicao
+--5-Quais clientes, com apólices ativas, que tiveram sinistros concluídos nos últimos 6 meses e cujo tipo de ocorrência foi 'Roubo' ou 'Dano', geraram mais despesas para a seguradora, considerando apenas os custos de reparos?
+SELECT 
+    Clientes.nome AS NomeCliente,
+    SUM(ISNULL(Reparos.custo_reparo, 0)) AS TotalDespesas
 FROM Clientes
 INNER JOIN Aparelhos ON Clientes.id_cliente = Aparelhos.id_cliente
 INNER JOIN Apolices ON Aparelhos.id_aparelho = Apolices.id_aparelho
-LEFT JOIN Sinistros ON Apolices.id_apolice = Sinistros.id_apolice
+INNER JOIN Sinistros ON Apolices.id_apolice = Sinistros.id_apolice
 LEFT JOIN Reparos ON Sinistros.id_sinistro = Reparos.id_sinistro
-LEFT JOIN Pagamentos ON Apolices.id_apolice = Pagamentos.id_apolice AND Pagamentos.status_pagamento = 'Pago'
-GROUP BY Clientes.id_cliente, Clientes.nome
-ORDER BY posicao;
+WHERE 
+    Apolices.status_apolice = 'Ativa'                     
+    AND Sinistros.status_sinistro = 'Concluído'          
+    AND Sinistros.data_ocorrencia >= DATEADD(MONTH, -6, GETDATE())  
+    AND Sinistros.tipo_ocorrencia IN ('Roubo', 'Dano')   
+GROUP BY Clientes.nome
+ORDER BY TotalDespesas DESC;
+
 
 --6-Crie uma procedure que receba um ID de cliente e retorne: Dados cadastrais, Total de apólices, Total de sinistros, Valor total pago, Valor total de reparos
 CREATE OR ALTER PROCEDURE sp_resumo_cliente @id_cliente INT AS
